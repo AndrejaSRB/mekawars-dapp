8f8591e90c1a95c345edd9331557c5836e37c113
AndrejaSRB <andreja.kojadinovic@gmail.com>
2023-02-05 13:51:37 +0100

    fix(context): remove duplicate imports

--- src/context/Web3Context.tsx
+++ src/context/Web3Context.tsx
@@ -1,8 +1,7 @@
-import type { ReactNode, ReactNode} from 'react';
-import { createContext, useContext } from 'react';
-import { useWeb3 } from '../hooks/Web3Client';
-import type { Web3ProviderState} from '../reducers';
+import { useWeb3 } from "../hooks/Web3Client";
+import type { Web3ProviderState } from "../reducers";
 import { web3InitialState } from "../reducers";
+import type { ReactNode } from "react";
 import { createContext, useContext } from "react";
 
 const Web3Context = createContext<Web3ProviderState>(web3InitialState);
